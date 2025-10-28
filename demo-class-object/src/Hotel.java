30385631e498de8f8eab04e28712cc5275baeae2
marvisman2695 <marvisman2695@gmail.com>
2025-10-29 01:33:58 +0800

    update

---
+++ demo-class-object/src/Hotel.java
@@ -0,0 +1,36 @@
+public class Hotel {
+
+  private String names;
+  private int rooms;
+
+  public Hotel() {
+    System.out.println("locating the hotel...");
+    this.names = "Ritz";
+    this.rooms = 171;
+  }
+  public static void main(String[] args) {
+
+      Hotel h1 = new Hotel();
+      System.out.println(h1.names);
+      System.out.println(h1.rooms);
+      
+
+      Hotel h2 = new Hotel();
+      System.out.println(h2.names);
+      System.out.println(h2.rooms);
+
+      h2.names = "Cordis";
+      h2.rooms = 665;
+      
+      Hotel[] hotels = new Hotel[] {h1, h2};
+      System.out.println(hotels[2].names);
+      System.out.println(hotels[2].rooms);
+    
+
+      // without Class
+      String[] names = new String[] {"Ritz","Cordis"};
+      int[] rooms = new int[] {171,665};
+
+    }
+  
+}
